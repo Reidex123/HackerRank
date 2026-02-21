9aa3a62e1e8a9a178e6d8518a408572437f1c784
Koketso Gaowelwe <gwlkok001@nightmare.cs.uct.ac.za>
2026-02-21 12:19:39 +0000

    recSeries problem

---
+++ recSeries.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class recSeries{
+	public static void recSeries1(int a, int b, int n-1){
+		System.out.printf("%d ", n);
+		if (n == 0){
+			return a + (Math.pow(2, n)*b);
+		}
+
+		 recSeries1(a,b,n)
+	}
+
+	public static void main(String[] args){
+		try(Scanner input = new Scanner(System.in))
+
+			int q = input.nextINt();
+
+			for (int i = 0; i < q; i++){
+				int a = input.nextInt();
+				int b = input.nextInt();
+				int n = input.nextInt();
+
+				recSeries1(a,b,n);
+			}
+	}	}
+}
