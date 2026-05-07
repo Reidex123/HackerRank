95da03570299883d2881391edd83c37f78e7ac96
Koketso <koketsogaowelwe2@gmail.com>
2026-05-07 12:01:16 +0200

    program to find permutaion of a word

---
+++ backTracking/Permutaion.java
@@ -0,0 +1,12 @@
+package backTracking;
+
+public class Permutaion {
+
+    public static void main(String[] args) {
+
+        try (java.util.Scanner input = new java.util.Scanner(System.in)) {
+
+            
+        }
+    }
+}
